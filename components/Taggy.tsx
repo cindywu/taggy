4d443e2a84b7767533cdda72df22d0dd6bb08b7a
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-09 19:45:36 -1000

    show a car and socket.io

---
+++ components/Taggy.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { Socket } from 'Socket.IO-client'
+import io from 'Socket.IO-client'
+let socket : Socket
+
+interface CarState {
+  x: number
+  y: number
+  angle: number
+  username: string
+}
+
+const Home = () => {
+  const [cars, setCars] = useState<CarState[]>([])
+  const [myCar, setMyCar] = useState<CarState>({ x: 0, y: 0, angle: 0, username: 'cindy' })
+
+  // position updated
+
+  // user joined
+    // car position
+
+  // user left
+
+  useEffect(() => void socketInitializer(), [])
+
+  // useEffect(() => {
+  //   document.addEventListener('keydown', (e) => {
+  //     if (e.key === 'ArrowRight') {
+  //       setMyCar({ ...myCar, x: myCar.x + 1 })
+  //     }
+  //     if (e.key === 'ArrowLeft') {
+  //       setMyCar({ ...myCar, x: myCar.x - 1 })
+  //     }
+  //     if (e.key === 'ArrowUp') {
+  //       setMyCar({ ...myCar, y: myCar.y - 1 })
+  //     }
+  //     if (e.key === 'ArrowDown') {
+  //       setMyCar({ ...myCar, y: myCar.y + 1 })
+  //     }
+  //   })
+
+  // },[myCar])
+
+  const [input, setInput] = useState('')
+
+  const onChangeHandler = (e) => {
+    // debugger;
+    setInput(e.target.value)
+    socket?.emit('input-change', e.target.value)
+  }
+
+  const socketInitializer = async () => {
+    await fetch('/api/hello')
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+      socket.emit('join', myCar)
+    })
+  }
+
+  return (
+    <div style={{position: 'relative'}}>
+      <div style={{transform: `translate(${myCar.x}, ${myCar.y})`, width: '50px', height: '25px', position: 'absolute', backgroundColor: 'blue'}}>
+
+      </div>
+    <input
+      placeholder="Type something"
+      value={input}
+      onChange={onChangeHandler}
+    />
+    </div>
+  )
+}
+
+export default function Taggy() {
+  return (
+    <div>Taggy
+      <Home></Home>
+    </div>
+  )
+}
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Socket } from 'dgram'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Server } from 'socket.io'
 
 type Data = {
   name: string
@@ -9,5 +11,21 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (res.socket && res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.emit('hello world', 'hello world')
+      socket.on('input-change', msg => {
+        console.log('i am in input change!')
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+
+  }
+  res.end()
 }
